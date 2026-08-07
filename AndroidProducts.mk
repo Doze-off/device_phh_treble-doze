0d90f2f31e9e7d684ef42203def5cf6c817b7e3b
Doze-off <viniciussadxxx@gmail.com>
2026-08-07 01:18:54 -0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,37 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/infinity_gsi_gapps.mk \
+	$(LOCAL_DIR)/infinity_gsi_vanilla.mk \
+	$(LOCAL_DIR)/infinity_a64_vanilla.mk \
+	$(LOCAL_DIR)/derpfest_gsi.mk \
+	$(LOCAL_DIR)/derpfest_gsi_mini.mk \
+	$(LOCAL_DIR)/witaqua_gsi_gapps.mk \
+	$(LOCAL_DIR)/witaqua_gsi_vanilla.mk \
+	$(LOCAL_DIR)/axion_gsi.mk \
+	$(LOCAL_DIR)/pixelage_gsi.mk \
+	$(LOCAL_DIR)/lunaris_gsi.mk \
+	$(LOCAL_DIR)/cherish_gsi.mk \
+	$(LOCAL_DIR)/crdroid_gsi.mk \
+	$(LOCAL_DIR)/evox_gsi.mk \
+	$(LOCAL_DIR)/genesis_gsi.mk \
+	$(LOCAL_DIR)/voltage_gsi.mk \
+	$(LOCAL_DIR)/lessaosp_gsi.mk \
+	$(LOCAL_DIR)/yaap_gsi.mk \
+	$(LOCAL_DIR)/rising_gsi.mk \
+	$(LOCAL_DIR)/everest_gsi.mk \
+	$(LOCAL_DIR)/alpha_gsi.mk \
+	$(LOCAL_DIR)/avium_gsi.mk \
+	$(LOCAL_DIR)/euclid_gsi.mk \
+	$(LOCAL_DIR)/superior_gsi.mk \
+	$(LOCAL_DIR)/luminedroid_gsi.mk \
+	$(LOCAL_DIR)/mica_gsi.mk \
+	$(LOCAL_DIR)/clover_gsi.mk \
+	$(LOCAL_DIR)/neoteric_gsi.mk \
+	$(LOCAL_DIR)/aicp_gsi.mk \
+	$(LOCAL_DIR)/pixelos_gsi.mk \
+	$(LOCAL_DIR)/lineage_gsi.mk \
+	$(LOCAL_DIR)/mistos_gsi.mk \
+	$(LOCAL_DIR)/sakura_gsi.mk \
+	$(LOCAL_DIR)/bliss_gsi.mk \
+	$(LOCAL_DIR)/lynx_gsi.mk \
+
+
