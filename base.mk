e32360e9e911145e5d1d711aa87ecfb15c8f4af1
Doze-off <viniciussadxxx@gmail.com>
2026-08-07 01:44:02 -0300

    Add apns-conf.xml to PRODUCT_PACKAGES
    Include apns-conf.xml in PRODUCT_PACKAGES within base.mk so the APN configuration file is packaged into the build output (ensuring APN definitions are present on the device).
    
    Co-Authored-By: Christopher A. Williamson <1141769+cawilliamson@users.noreply.github.com>

--- base.mk
+++ base.mk
@@ -24,6 +24,8 @@ $(call inherit-product, vendor/hardware_overlay/overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+PRODUCT_PACKAGES += apns-conf.xml
+
 # Those overrides are here because Huawei's init read properties
 # from /system/etc/prop.default, then /vendor/build.prop, then /system/build.prop
 # So we need to set our props in prop.default
