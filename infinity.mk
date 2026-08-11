efab8dc52de96aa110ab67eca917bad79d2769f0
Doze-off <viniciussadxxx@gmail.com>
2026-08-11 01:35:19 -0300

    Add infinity device and GSI makefiles
    Add device configuration for 'infinity': infinity.mk sets vendor/board inheritance and device-specific build flags (boot animation, blur, GApps, UDFPS, bypass charge), maintainer and build type. Add infinity_gsi.mk to define an arm64 GSI product (inheriting base and infinity), set product properties, mark device characteristics, and enable GApps for the GSI build.

---
+++ infinity.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, vendor/infinity/config/common.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/infinity/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/infinity/build/core/config.mk
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_BLUR := true
+TARGET_SHIPS_FULL_GAPPS := false
+TARGET_HAS_UDFPS := false
+BYPASS_CHARGE_SUPPORTED := true
+
+INFINITY_MAINTAINER := Doze-off
+INFINITY_BUILD_TYPE := OFFICIAL
+
---
+++ infinity_gsi.mk
@@ -0,0 +1,20 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, device/phh/treble/infinity.mk)
+
+PRODUCT_NAME := infinity_gsi
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MANUFACTURER := google
+PRODUCT_SYSTEM_MANUFACTURER := google
+
+PRODUCT_MODEL := Infinity X GSI TrebleDroid
+LINEAGE_BUILD := infinity
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+WITH_GAPPS := true
+
