4537985a496af72c9eb0cb458eda7ce6e3048339
Doze-off <viniciussadxxx@gmail.com>
2026-08-11 01:49:25 -0300

    Add sakura and sakura_gsi product makefiles
    Introduce sakura.mk and sakura_gsi.mk to define a Project Sakura GSI product. sakura.mk inherits Lineage and device sepolicy/BoardConfig files and sets TARGET_BOOT_ANIMATION_RES, TARGET_HAS_UDFPS and SAKURA_MAINTAINER. sakura_gsi.mk configures an arm64 GSI product (sakura_gsi), includes aosp_arm64 and device/phh/treble base/sakura, and sets product metadata (brand/manufacturer/model), LINEAGE_BUILD and SAKURA_BUILD_TYPE=gapps.

---
+++ sakura.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_HAS_UDFPS:= false
+
+SAKURA_MAINTAINER := Doze-off
